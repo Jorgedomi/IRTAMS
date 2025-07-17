0d8a3cec409d98d8b266da5c6d029fddbd3e5ffe
Jorgedomi <148155638+Jorgedomi@users.noreply.github.com>
2025-07-17 02:24:02 -0500

    Add files via upload

---
+++ validar.php
@@ -0,0 +1,43 @@
+<?php
+session_start(); //Iniciar sesión
+
+$servidor = "localhost";
+$usuario = "root";
+$contrasena = "";
+$base_de_datos = "materias";
+
+
+$conn = new mysqli($servidor, $usuario, $contrasena, $base_de_datos);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+// 📥 Obtener datos del formulario
+$usu = $_POST["usuario"];
+$contra = $_POST["password"];
+
+// 🔎 Consulta para validar usuario y contraseña
+$sql = "SELECT id_usuario, nombre, password FROM usuario WHERE nombre = '$usu' AND password = '$contra'";
+$resultado = $conn->query($sql);
+
+if ($resultado->num_rows > 0) {
+    $fila = $resultado->fetch_assoc();
+
+    //Guardar id_usuario en la sesión
+    $_SESSION["id_usuario"] = $fila["id_usuario"];
+
+    echo "<script>
+                    alert('✅ Inicio de sesion correcta.');
+                    window.location.href = 'bienvenido.php';
+                 </script>";
+    exit();
+} else {
+    echo "<script>
+                    alert('Inicio de sesion incorrecta.');
+                    window.location.href = 'index.php';
+                 </script>";
+}
+
+$conn->close();
+?>
